6041eb0ec85c23e3fbabd2123754381dc63d1dee
Sourav Saha <souravsaha101@gmail.com>
2021-09-27 20:56:02 +0530

    code chnages

---
+++ frontend/components/Page.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react/prop-types
+export default function Page({ children }) {
+  return (
+    <>
+      <h1>Page Comp</h1>
+      {children}
+    </>
+  );
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SellPage() {
+  return (
+    <>
+      <h1>Sell Page</h1>
+    </>
+  );
+}
